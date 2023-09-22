8385408d026c7bf80b2f1fbfddf8b977dcd4bd0b
Nicolas Boiteau <nicolas.boiteau@efrei.net>
2023-09-22 23:38:41 +0200

    html+css

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const path = require('path')
-console.log("hey hey! This is the first assignement of the web course.")
+const path = require('path');
+const port = process.env.PORT || 3500;
+
+app.use(express.static('public'));
+// Middleware to disable caching
+app.use((req, res, next) => {
+    res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.header('Pragma', 'no-cache');
+    res.header('Expires', '0');
+    next();
+});
+
+// Define the directory containing your static files (CSS, JS, images, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define the route to serve the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'HTML', 'index.html'));
+});
+app.listen(port, () => {
+    console.log(`Serveur en cours d'exécution sur le port ${port}`);
+  });
