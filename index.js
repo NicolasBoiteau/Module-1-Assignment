147c152b296aaeb3d8f3c90792654c3e2e05d41f
Nicolas Boiteau <nicolas.boiteau@efrei.net>
2023-09-22 02:45:49 +0200

    added html

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path')
 console.log("hey hey! This is the first assignement of the web course.")
